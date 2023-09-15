7ab981682ea029535685b86de259ed68cc31322c
GuangyanS <george20020904@gmail.com>
2023-09-15 17:36:53 +0800

    proj 0 change class body to class planet

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -10,18 +10,18 @@ public class NBody {
         // TODO Not robust enough if we have planet numbers other than 5
         In in = new In(filename);
         in.readInt(); in.readDouble();
-        Planet[] bodies = new Planet[5];;
+        Planet[] Planets = new Planet[5];;
         for(int i = 0; i < 5; i++){
-            bodies[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readString());
+            Planets[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readString());
         }
-        return bodies;
+        return Planets;
     }
     public static void main(String[] args){
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
         double radius = readRadius(filename);
-        Planet[] bodies = readPlanets(filename);
+        Planet[] Planets = readPlanets(filename);
 
         // Create an xForces array and yForces array.
         double[] xForces = new double[5];
@@ -34,30 +34,30 @@ public class NBody {
             // Calculate the net x and y forces for each Body,
             // storing these in the xForces and yForces arrays respectively.
             for(int i = 0; i < 5; i++){
-                xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
-                yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
+                xForces[i] = Planets[i].calcNetForceExertedByX(Planets);
+                yForces[i] = Planets[i].calcNetForceExertedByY(Planets);
             }
             // After calculating the net forces for every Body, call update on each of the Bodys.
             // This will update each body’s position, velocity, and acceleration.
             for(int i = 0; i < 5; i++){
-                 bodies[i].update(dt, xForces[i], yForces[i]);
+                Planets[i].update(dt, xForces[i], yForces[i]);
             }
             // Draw the background image
             StdDraw.picture(0, 0, "images/starfield.jpg");
             // Draw all of the Body s.
             for(int i = 0; i < 5; i++){
-                StdDraw.picture(bodies[i].xxPos, bodies[i].yyPos, "images/" + bodies[i].imgFileName);
+                StdDraw.picture(Planets[i].xxPos, Planets[i].yyPos, "images/" + Planets[i].imgFileName);
             }
             StdDraw.show();
             StdDraw.pause(10);
         }
         StdDraw.clear();
-        StdOut.printf("%d\n", bodies.length);
+        StdOut.printf("%d\n", Planets.length);
         StdOut.printf("%.2e\n", radius);
-        for (int i = 0; i < bodies.length; i++) {
+        for (int i = 0; i < Planets.length; i++) {
             StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                    bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
-                    bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);
+                    Planets[i].xxPos, Planets[i].yyPos, Planets[i].xxVel,
+                    Planets[i].yyVel, Planets[i].mass, Planets[i].imgFileName);
         }
     }
 }
