2df798cdea9c18c676f3f98ac9f42ea3d7441a40
GuangyanS <george20020904@gmail.com>
2023-09-27 12:45:06 +0800

    proj1a revised v5 for style

--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -29,43 +29,17 @@ public class ArrayDequeTest {
         assertEquals(36, L.get(1).intValue());
     }
     /** Tests insertion of a large number of items.*/
-//    @Test
-//    public void testMegaInsert() {
-//        ArrayDeque<Integer> L = new ArrayDeque<Integer>();
-//        int N = 1000000;
-//        for (int i = 0; i < N; i += 1) {
-//            L.addLast(i);
-//        }
-//        for (int i = 0; i < N; i += 1) {
-//            L.addLast(L.get(i));
-//        }
-//    }
     @Test
-    public void testRemove() {
+    public void testMegaInsert() {
         ArrayDeque<Integer> L = new ArrayDeque<Integer>();
-        L.addLast(0);
-        L.addLast(1);
-        L.addFirst(2);
-        L.addLast(3);
-        L.get(3)      ;
-        L.removeFirst();
-        L.addFirst(6);
-        L.addLast(7);
-        L.removeFirst() ;
-        L.addLast(9);
-        L.addFirst(10);
-        L.addFirst(11);
-        L.addLast(12);
-        L.addFirst(13);
-        L.addLast(14);
-        L.removeLast()   ;
-        L.addLast(16);
-        L.addLast(17);
-        L.addFirst(18);
-        int k = L.get(4) ;
-
+        int N = 1000000;
+        for (int i = 0; i < N; i += 1) {
+            L.addLast(i);
+        }
+        for (int i = 0; i < N; i += 1) {
+            L.addLast(L.get(i));
+        }
     }
-
     public static void main(String[] args) {
         jh61b.junit.TestRunner.runTests("all", ArrayDequeTest.class);
     }
