dfaec9768db216346f5d2bacf3c41e949c3b8b93
GuangyanS <george20020904@gmail.com>
2023-09-27 11:16:45 +0800

    proj1a

---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,77 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/** Tests the ArrayDeque class.
+ *  @author Josh Hug
+ *  Modified by George Sun
+ */
+
+public class ArrayDequeTest {
+    @Test
+    public void testEmptySize() {
+        ArrayDeque<Integer> L = new ArrayDeque<Integer>();
+        assertEquals(0, L.size());
+    }
+
+    @Test
+    public void testAddAndSize() {
+        ArrayDeque<Integer> L = new ArrayDeque<Integer> ();
+        L.addLast(99);
+        L.addLast(99);
+        assertEquals(2, L.size());
+    }
+
+
+    @Test
+    public void testAddAndGetLast() {
+        ArrayDeque<Integer> L = new ArrayDeque<Integer> ();
+        L.addLast(99);
+        assertEquals(99, L.getLast().intValue());
+        L.addLast(36);
+        assertEquals(36, L.getLast().intValue());
+    }
+
+
+    @Test
+    public void testGet() {
+        ArrayDeque<Integer> L = new ArrayDeque<Integer> ();
+        L.addLast(99);
+        assertEquals(99, L.get(0).intValue());
+        L.addLast(36);
+        assertEquals(99, L.get(0).intValue());
+        assertEquals(36, L.get(1).intValue());
+    }
+
+
+    @Test
+    public void testRemove() {
+        ArrayDeque<Integer> L = new ArrayDeque<Integer> ();
+        L.addLast(99);
+        assertEquals(99, L.get(0).intValue());
+        L.addLast(36);
+        assertEquals(99, L.get(0).intValue());
+        L.removeLast();
+        assertEquals(99, L.getLast().intValue());
+        L.addLast(100);
+        assertEquals(100, L.getLast().intValue());
+        assertEquals(2, L.size());
+    }
+
+    /** Tests insertion of a large number of items.*/
+    @Test
+    public void testMegaInsert() {
+        ArrayDeque<Integer> L = new ArrayDeque<Integer> ();
+        int N = 1000000;
+        for (int i = 0; i < N; i += 1) {
+            L.addLast(i);
+        }
+
+        for (int i = 0; i < N; i += 1) {
+            L.addLast(L.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        jh61b.junit.TestRunner.runTests("all", ArrayDequeTest.class);
+    }
+}
